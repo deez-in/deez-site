59f9b5017bfb7a0b5bec0695ad50f9771f746aed
debarkamondal <debarkamondal@gmail.com>
2026-09-19 13:15:11 +0530

    feat: added icon, logo and opengraph images

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://deez.in',
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  integrations: [sitemap()]
 });
